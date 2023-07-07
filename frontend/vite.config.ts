24de873e92eddc60007bef65b630ed802a65e533
AlgeMoya <kjhobd2@naver.com>
2023-07-07 14:49:44 +0900

    fix: Docker Compose 문제 해결

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,27 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // host: '0.0.0.0',
+    host: true,
+  }
 })
+
+// import tsconfigPaths from "vite-tsconfig-paths";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import path from "path";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tsconfigPaths()],
+//   server: {
+//     port: 3000,
+    
+//   },
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// });
