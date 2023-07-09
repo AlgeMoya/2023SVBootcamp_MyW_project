f7be486be45d505c2f53f96cad584e8a23f37b5d
김연진 <love_homebody@naver.com>
2023-07-10 01:18:23 +0900

    feat: #14 내비게이션 바 수정

--- frontend/src/components/NavigationBar.tsx
+++ frontend/src/components/NavigationBar.tsx
@@ -4,40 +4,46 @@ import NavigationBarBtn from './NavigationBarBtn';
 export default function NavigationBar() {
     return(
         <>
-            <div className="w-[1312px] h-16 absolute left-[99.5px] top-[13.5px] rounded-bl-20 rounded-br-20 bg-[#BCBAB8]" />
-            <div className="w-screen h-3.5 absolute left-[-0.5px] top-0 bg-beige" />
-            <div className="w-[251px] h-16 absolute left-[100px] top-[13.5px] rounded-bl-20 bg-beige" />
-            <div className="w-[251px] h-16 absolute left-[1160.5px] top-[13.5px] rounded-br-20 bg-beige" />
-
-            <div className="w-[1160px] h-15">
-                <p className="absolute left-[184px] top-15 text-3xl font-medium text-center text-white">
-                    MyW
-                </p>
-                <div className="absolute left-[452px]">
-                    <NavigationBarBtn link='/'>
-                        Home
-                    </NavigationBarBtn>
-                </div>
-                <div className="absolute left-[607px]">
-                    <NavigationBarBtn link='/mypage'>
-                        My page
-                    </NavigationBarBtn>
-                </div>
-                <div className="absolute left-[791px]">
-                    <NavigationBarBtn link='/bookshelf'>
-                        Bookshelf
-                    </NavigationBarBtn>
-                </div>
-                <div className="absolute left-[996px]">
-                    <NavigationBarBtn link='/login'>
-                        Log in
-                    </NavigationBarBtn>
-                </div>
-                <div className="absolute w=[114px] left-[1230px] top-2.5 text-2xl font-medium text-right text-white">
-                        Make your<br/>
-                        World
+            <div className="w-5/6 h-16 top-3.5 absolute">
+                <div className='flex'>
+                    <div className='flex-1'>
+                        <div className="w-2/12 h-full py-4 flex-1 absolute text-3xl font-medium text-center text-white rounded-bl-20 bg-beige">
+                            MyW
+                        </div>
+                    </div>
+                    <div className='w-4/6 h-16 bg-[#BCBAB8] flex flex-row items-center'>
+                        
+                        <div className='flex-1 flex flex-col items-center'>
+                            <NavigationBarBtn link='/'>
+                                Home
+                            </NavigationBarBtn>
+                        </div>
+                        <div className='flex-1 flex flex-col items-center'>
+                            <NavigationBarBtn link='/mypage'>
+                                My page
+                            </NavigationBarBtn>
+                        </div>
+                        <div className='flex-1 flex flex-col items-center'>
+                            <NavigationBarBtn link='/bookshelf'>
+                                Bookshelf
+                            </NavigationBarBtn>
+                        </div>
+                        <div className='flex-1 flex flex-col items-center'>
+                            <NavigationBarBtn link='/login'>
+                                Log in
+                            </NavigationBarBtn>
+                        </div>
+                        
+                    </div>
+                    <div className='w-2/12 h-full flex-1 rounded-br-20 text-right text-2xl text-white bg-beige'>
+                        <span className='flex flex-col items-center'>
+                            Make your<br/>
+                            World
+                        </span>
+                    </div>
                 </div>
             </div>
+            <div className="w-screen h-3.5 absolute left-0 top-0 bg-beige" />
         </>
     );
 }
