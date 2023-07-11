ac8bcc210511fc42a03a85508b3b2dc76a2d5f2c
HaeunKim01 <133198762+HaeunKim01@users.noreply.github.com>
2023-07-11 22:39:36 +0900

    로그인페이지

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+
 import './App.css';
 
 import ChoicePage from './pages/ChoicePage';
 import NavigationBar from './components/NavigationBar';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -12,9 +14,10 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/choice" element={<ChoicePage />} />
+          <Route path="/login" element={<Login/>} />
         </Routes>
       </div>
-      <NavigationBar />
+
     </BrowserRouter>
   );
 }
