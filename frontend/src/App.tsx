d04078bf2cad190bd97fdd24608fd87419da3533
HaeunKim01 <133198762+HaeunKim01@users.noreply.github.com>
2023-07-12 01:14:28 +0900

    feat: 회원가입 페이지 퍼블리싱

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import './App.css';
 
 import ChoicePage from './pages/ChoicePage';
 import NavigationBar from './components/NavigationBar';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/choice" element={<ChoicePage />} />
+          <Route path="/SignUp" element={<SignUp />} />
         </Routes>
       </div>
       <NavigationBar />
