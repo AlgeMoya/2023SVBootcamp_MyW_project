9a46534df909ef741fa2fbb950b634ba7cad672f
AlgeMoya <kjhobd2@naver.com>
2023-07-14 00:13:39 +0900

    Feat: 마이페이지 퍼블리싱, 테일윈드 전환 완료

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
+import tailwindcssanimate from "tailwindcss-animate";
 
 const px0_10 = { ...Array.from(Array(11)).map((_, i) => `${i}px`) };
 const px0_100 = { ...Array.from(Array(101)).map((_, i) => `${i}px`) };
 const px0_200 = { ...Array.from(Array(201)).map((_, i) => `${i}px`) };
-const defaultTheme = require("tailwindcss/defaultTheme");
+
+// const defaultTheme = require("tailwindcss/defaultTheme");
 
 export default {
   content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
@@ -16,11 +19,14 @@ export default {
         beige: "#9b8f8f",
         "beige-white": "#fdf9f6",
         choice: "#fde6d5",
+        mainpagegray: "#a59c9b",
+        paginationdeepgray: "#d0cfcd",
+        paginationlightgray: "#eae9e9",
       },
       fontFamily: {
         sans: ["IBMPlexSansKR"],
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssanimate],
 };
