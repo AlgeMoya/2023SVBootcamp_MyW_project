034697753a09a23588eb4a4f1be8b22f67716707
test1 <test>
2023-07-18 00:24:32 +0900

    feat

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -16,7 +16,6 @@ import environ
 import os
 
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -44,9 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'openapi',
-    'rest_framework'
-
+    "openapi",
 ]
 
 MIDDLEWARE = [
@@ -144,4 +141,3 @@ DATABASES = {
 AUTH_USER_MODEL = "users.MyUser"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
-
--- backend/openapi/models.py
+++ backend/openapi/models.py
@@ -1,33 +1,47 @@
 from django.db import models
 from users.models import MyUser
+
+
 class Novel(models.Model):
-    user_id = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='user_novel')
+    user_id = models.ForeignKey(
+        MyUser, on_delete=models.CASCADE, related_name="user_novel"
+    )
     novel_name = models.CharField(max_length=100)
     novel_image = models.CharField(max_length=255)
     status = models.BooleanField(default=True)
     create_at = models.DateTimeField(auto_now_add=True)
     udate_at = models.DateTimeField(auto_now=True)
     delete_at = models.DateTimeField(null=True)
+
     def __str__(self):
         return self.novel_name
-    
+
+
 class ChatLog(models.Model):
-    novel_id = models.ForeignKey(Novel, on_delete=models.CASCADE, related_name='novel_chatlog')
+    novel_id = models.ForeignKey(
+        Novel, on_delete=models.CASCADE, related_name="novel_chatlog"
+    )
     chat_log = models.TextField()
     create_at = models.DateTimeField(auto_now_add=True)
     udate_at = models.DateTimeField(auto_now=True)
     delete_at = models.DateTimeField(null=True)
 
+
 class Character(models.Model):
-    novel_id = models.ForeignKey(Novel, on_delete=models.CASCADE, related_name='novel_character')
+    novel_id = models.ForeignKey(
+        Novel, on_delete=models.CASCADE, related_name="novel_character"
+    )
     name = models.CharField(max_length=20)
     personality = models.CharField(max_length=100)
     create_at = models.DateTimeField(auto_now_add=True)
     udate_at = models.DateTimeField(auto_now=True)
     delete_at = models.DateTimeField(null=True)
 
+
 class NovelStory(models.Model):
-    novel_id = models.ForeignKey(Novel, on_delete=models.CASCADE, related_name='novel_story')
+    novel_id = models.ForeignKey(
+        Novel, on_delete=models.CASCADE, related_name="novel_story"
+    )
     page = models.IntegerField()
     content = models.TextField()
     image = models.CharField(max_length=100)
@@ -35,8 +49,11 @@ class NovelStory(models.Model):
     udate_at = models.DateTimeField(auto_now=True)
     delete_at = models.DateTimeField(null=True)
 
+
 class Background(models.Model):
-    novel_id = models.ForeignKey(Novel, on_delete=models.CASCADE, related_name='novel_background')
+    novel_id = models.ForeignKey(
+        Novel, on_delete=models.CASCADE, related_name="novel_background"
+    )
     genre = models.CharField(max_length=100)
     time_period = models.CharField(max_length=100)
     time_projection = models.CharField(max_length=100)
