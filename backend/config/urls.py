5fe631cd63ebe3d6bec61db42ba6a8bb2e68087e
test1 <test>
2023-07-18 17:27:56 +0900

    fix : url 변경

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,8 +1,6 @@
-
 from django.contrib import admin
 from django.urls import path, include
 from openapi import views
-<<<<<<< HEAD
 from drf_yasg.views import get_schema_view
 from users import user_views
 from drf_yasg import openapi
@@ -19,8 +17,6 @@ schema_view = get_schema_view(
     public=True,
     permission_classes=(permissions.AllowAny,),
 )
-=======
->>>>>>> 6e3f3225c61d5bfbbc55a468dfcbd44732d01e58
 
 # from django.contrib.auth import views as auth_views
 from users import views
@@ -28,7 +24,6 @@ from users import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("user/", include("users.urls")),
-<<<<<<< HEAD
     path("", user_views.index, name="index"),  # '/' 에 해당되는 path
     path("input_form/", views.input_form, name="input_form"),
     path("chat/", views.chat, name="chat"),
@@ -50,11 +45,4 @@ urlpatterns = [
         schema_view.without_ui(cache_timeout=0),
         name="schema-swagger-ui-config",
     ),
-=======
-    path("openapi/", include("openapi.urls")),
-    path("", views.index, name="index"),  # '/' 에 해당되는 path
-    path('input_form/', views.input_form, name='input_form'),
-    path('chat/', views.chat, name='chat'),
->>>>>>> 6e3f3225c61d5bfbbc55a468dfcbd44732d01e58
 ]
-
