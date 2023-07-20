bcde2b5b5f4e32066fa2e3bfdfacb4909824990b
josephuk77 <josephuk77@gmail.com>
2023-07-20 14:53:08 +0900

    fix:#7 url 주석제거

--- backend/openapi/urls.py
+++ backend/openapi/urls.py
@@ -7,7 +7,7 @@ app_name = "openapi"
 urlpatterns = [
     path("novels/<int:novel_id>", views.input_form, name="novel_input"),
     path("mynovels", views.novel_list, name="novel_list"),
-    #path("mynovels/<int:novel_id>", views.mynovels, name="novel_delete"),
+    path("mynovels/<int:novel_id>", views.mynovels, name="novel_delete"),
     path("get_parsed_result/<int:novel_id>", views.get_parsed_result),
     path("input_form/<int:novel_id>", views.input_form)
 ]
