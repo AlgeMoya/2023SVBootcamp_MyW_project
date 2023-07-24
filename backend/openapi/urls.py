69c6634181ba49b613336516f8370c109fdf796e
josephuk77 <josephuk77@gmail.com>
2023-07-24 16:00:48 +0900

    feat:#93 페이징 완료

--- backend/openapi/urls.py
+++ backend/openapi/urls.py
@@ -6,6 +6,7 @@ app_name = "openapi"
 
 urlpatterns = [
     path("novels/<int:novel_id>", views.NovelView.as_view(), name="novel_input"),
-    path("mynovels", views.novel_list, name="novel_list"),
+    path("mynovels", views.mynovel_list, name="novel_list"),
     path("mynovels/<int:novel_id>", views.mynovels, name="novel_delete"),
+    path("novels", views.novel_list)
 ]
