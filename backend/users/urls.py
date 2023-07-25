6e7b09b9177639c9689e55cac39b593cc56dd6b7
josephuk77 <josephuk77@gmail.com>
2023-07-25 17:27:01 +0900

    fix: 오류해결

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from users.views import index, RegistrationAPIView, LoginAPIView
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
+
 
 urlpatterns = [
     path("", index),
@@ -11,20 +9,4 @@ urlpatterns = [
         LoginAPIView.as_view(),
     ),
     path("sign/", RegistrationAPIView.as_view()),
-    path(
-        "swagger/<str:format>/",
-        schema_view.without_ui(cache_timeout=0),
-        name="schema-json",
-    ),
-    path(
-        "swagger/",
-        schema_view.with_ui("swagger", cache_timeout=0),
-        name="scehma-swagger-ui",
-    ),
-    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
-    path(
-        "swagger/swagger-config.js",
-        schema_view.without_ui(cache_timeout=0),
-        name="schema-swagger-ui-config",
-    ),
 ]
